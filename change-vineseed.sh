5f295b5bf5fcb91d4c926e8c6093c7d11b58dd4d
hiroyuki12 <hiroyuki12@gmail.com>
2021-04-29 03:03:51 +0900

    Add change-vineseed.sh.

---
+++ change-vineseed.sh
@@ -0,0 +1,4 @@
+sed -i -e 's/6.5/VineSeed/g' /etc/apt/sources.list.d/main.list
+sed -i -e 's/6.5/VineSeed/g' /etc/apt/sources.list.d/plus.list
+sed -i -e 's/6.5/VineSeed/g' /etc/apt/sources.list.d/nonfree.list
+
