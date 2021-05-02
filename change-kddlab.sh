ddeab24d9688f2332943529734da345458b09dba
hiroyuki12 <hiroyuki12@gmail.com>
2021-05-02 11:25:00 +0900

    Add change-kddlab.sh, change-master.sh.

---
+++ change-kddlab.sh
@@ -0,0 +1,4 @@
+sed -i -e 's/updates.vinelinux.org/ftp.kddlabs.co.jp\/pub\/Linux\/distributions\/Vine/g' /etc/apt/sources.list.d/main.list
+sed -i -e 's/updates.vinelinux.org/ftp.kddlabs.co.jp\/pub\/Linux\/distributions\/Vine/g' /etc/apt/sources.list.d/plus.list
+sed -i -e 's/updates.vinelinux.org/ftp.kddlabs.co.jp\/pub\/Linux\/distributions\/Vine/g' /etc/apt/sources.list.d/nonfree.list
+
---
+++ change-master.sh
@@ -0,0 +1,4 @@
+sed -i -e 's/ftp.kddlabs.co.jp\/pub\/Linux\/distributions\/Vine/updates.vinelinux.org/g' /etc/apt/sources.list.d/main.list
+sed -i -e 's/ftp.kddlabs.co.jp\/pub\/Linux\/distributions\/Vine/updates.vinelinux.org/g' /etc/apt/sources.list.d/plus.list
+sed -i -e 's/ftp.kddlabs.co.jp\/pub\/Linux\/distributions\/Vine/updates.vinelinux.org/g' /etc/apt/sources.list.d/nonfree.list
+
