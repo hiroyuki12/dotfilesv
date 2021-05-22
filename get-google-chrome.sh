77e10774591f28c9e59378d33930bca51c85ea87
hiroyuki12 <hiroyuki12@gmail.com>
2021-05-22 13:19:27 +0900

    Update get-google-chrome.sh

--- get-google-chrome.sh
+++ get-google-chrome.sh
@@ -1,2 +1,5 @@
+which wget > /dev/null || sudo apt-get install wget
+
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+
 sudo apt-get install google-chrome-stable_current_x86_64.rpm
