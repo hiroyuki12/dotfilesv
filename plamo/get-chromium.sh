29b7eb6e950f3fb690da84d2d58e82acf59921b9
hiroyuki12 <hiroyuki12@gmail.com>
2021-05-29 21:09:40 +0900

    Update get-chromium.sh.

--- plamo/get-chromium.sh
+++ plamo/get-chromium.sh
@@ -1,3 +1,3 @@
-wget http://repository.plamolinux.org/pub/linux/Plamo/Plamo-7.x/x86_64/contrib/network/chromium90.0.4430.93-x86_64-B1.txz
+wget http://repository.plamolinux.org/pub/linux/Plamo/Plamo-7.x/x86_64/contrib/network/chromium-90.0.4430.93-x86_64-B1.txz
 
 sudo installpkg chromium90.0.4430.93-x86_64-B1.txz
