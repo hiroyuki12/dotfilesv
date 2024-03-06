11b7b5e3567226b7f06292c96eb89b4134c5d2ab
hiroyuki12 <hiroyuki12@gmail.com>
2024-03-06 22:27:43 +0900

    Update add-app.sh.

--- add-app.sh
+++ add-app.sh
@@ -1,1 +1,1 @@
-sudo apt install vim task-xfce lightdm firefox rust php83 golang nodejs rbenv build-essential gcc perl ruby lua redis chromium smplayer ruby-devel cifs-utils
+sudo apt install vim task-xfce lightdm firefox rust php83 golang nodejs rbenv build-essential gcc perl ruby lua redis chromium smplayer ruby-devel cifs-utils qt4-designer qt4-devel
