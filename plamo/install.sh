8f4b119665e4242811de0a1f1dc9594230b4c3cf
hiroyuki12 <hiroyuki12@gmail.com>
2024-03-07 16:27:32 +0900

    Update install.sh.

--- plamo/install.sh
+++ plamo/install.sh
@@ -1,5 +1,40 @@
 #!/usr/bin/env bash
 
+echo 'plamo install.sh'
+
+while true; do
+read -p 'Now Install Homebrew? [Y/n]' Answer
+case $Answer in
+  '' | [Yy]* )
+
+#install Homebrew
+which /home/linuxbrew/.linuxbrew/bin/brew > /dev/null
+if [ $? -ne 0 ]; then
+  sudo ln -s /usr/bin/touch /bin/touch
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo '# Homebrew' >> /home/hiroyuki/.bash_profile
+  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/hiroyuki/.bash_profile
+  eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+fi
+
+/home/linuxbrew/.linuxbrew/bin/brew -v
+
+# brew help
+# brew update
+# brew doctor
+# brew install wget
+
+break;
+;;
+[Nn]* )
+  echo "Skip Install Homebrew"
+  break;
+  ;;
+* )
+  echo Please answer YES or NO.
+esac
+done;
+
 while true; do
 read -p 'Now Initial Setting? [Y/n]' Answer
 case $Answer in
@@ -23,36 +58,6 @@ break;
 esac
 done;
 
-
-while true; do
-read -p 'Now Install Homebrew? [Y/n]' Answer
-case $Answer in
-  '' | [Yy]* )
-
-#install Hombrew
-which /home/linuxbrew/.linuxbrew/bin/brew > /dev/null
-if [ $? -ne 0 ]; then
-  sudo ln -s /usr/bin/touch /bin
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo '# Homebrew' >> ~/.bashrc
-  echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
-fi
-
-/home/linuxbrew/.linuxbrew/bin/brew -v
-
-#brew reinstall ruby-build
-
-
-break;
-;;
-[Nn]* )
-  echo "Skip Install Homebrew"
-  break;
-  ;;
-* )
-  echo Please answer YES or NO.
-esac
-done;
 
 while true; do
 read -p 'Now Install rbenv & serverkit? [Y/n]' Answer
